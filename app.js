0a32b91f9a26cc6f11372580303d8e4d26547b01
Jessica <51422758+Ellehcimisey@users.noreply.github.com>
2022-09-01 19:40:52 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -6,34 +6,65 @@ class Calculator {
     }
     //CE button, clears the view
     clear(){ 
+        this.currentCalculation = ""; 
         this.previousCalculation = "";
-        this.currentCalculation = "";
         this.action = undefined;
     }
     //so it doesn't add or change numbers 
     addNumber(number){ 
-        if( number === "." && this.currentCalculation.includes(".")) return;
+        if( number === "." && this.currentCalculation.includes(".")) return
         this.currentCalculation = this.currentCalculation.toString() + number.toString();
     }
     // + - / 
     addAction(action){ 
-        this.action = action;
-        this.previousCalculation = this.currentCalculation;
-        this.currentCalculation = "";
+        if(this.currentCalculation === "") return
+        if(this.previousCalculation !== ""){
+            this.calculate()
+        }
+        this.action = action
+        this.previousCalculation = this.currentCalculation
+        this.currentCalculation = ""
     }
     // = button
     calculate(){ 
-
+        let calculation
+        const previous = parseFloat(this.previousCalculation);
+        const current = parseFloat(this.currentCalculation);
+        if (isNaN(previous) || isNaN(current)) return;
+        switch (this.action){
+            case "+":
+                calculation = previous + current
+                break;
+            case "-":
+                calculation = previous - current
+                break;
+            case "÷":
+                calculation = previous / current
+                break;
+            case "x":
+                calculation = previous * current
+                break;
+            default:
+                return
+        }
+        this.currentCalculation = calculation;
+        this.action = undefined;
+        this.previousCalculation = " ";
     }
+    
     updateView(){
         this.current_div.innerHTML = this.currentCalculation;
+        if(this.action != null) {
+            this.previous_div.innerHTML = `${this.previousCalculation} ${this.action}`;
+        } else {
+            this.previous_div.innerHTML = "";
+        }
     }
-
 };
 
 
 //all buttons
-const action_btn = document.querySelectorAll("[data-acton]");
+const action_btn = document.querySelectorAll("[data-action]");
 const number_btn = document.querySelectorAll("[data-number]");
 const ce_btn = document.querySelector("[data-clear-entry]");
 const equal_btn = document.querySelector("[data-equal]");
@@ -44,30 +75,26 @@ const calculator = new Calculator(previous_div, current_div);
 
 
 //buttons with assigned actions
-action_btn.forEach(button => {
+action_btn.forEach( button => {
     button.addEventListener('click', ()=>{
         calculator.addAction(button.innerHTML);
         calculator.updateView();
     })
 });
 
-number_btn.forEach(button => {
+number_btn.forEach( button => {
     button.addEventListener('click', ()=>{
         calculator.addNumber(button.innerHTML);
         calculator.updateView();
     })
 });
 
-ce_btn.forEach(button => {
-    button.addEventListener('click', ()=>{
+ce_btn.addEventListener('click', ()=>{
         calculator.clear();
         calculator.updateView();
-    })
 });
 
-equal_btn.forEach(button => {
-    button.addEventListener('click', ()=>{
+equal_btn.addEventListener('click', ()=>{
         calculator.calculate();
         calculator.updateView();
-    })
 });
